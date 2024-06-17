ad85b38616023533beac5b3af6fb5786141110dc
Matos52 <Matej.Regec@gmail.com>
2024-06-17 21:58:49 +0200

    Adding new components.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
-import logo from '../assets/logo.jpg'
+import logo from "../assets/logo.jpg";
+import Button from "./UI/Button";
 
 export default function Header() {
   return (
     <header id="main-header">
       <div id="title">
-        <img src={logo} alt='React food logo' />
+        <img src={logo} alt="React food logo" />
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <button>Cart(0)</button>
+        <Button textOnly>Cart(0)</Button>
       </nav>
     </header>
   );
