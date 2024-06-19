1d2d399ffbd395f6322eb716de0ab045dae7fdba
Matos52 <Matej.Regec@gmail.com>
2024-06-19 21:43:54 +0200

    Adding new components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals";
 import React from "react";
+import { CartContextProvider } from "./components/store/CartContext.jsx";
 
 function App() {
   return (
-    <>
+    <CartContextProvider>
       <Header />
       <Meals />
-    </>
+    </CartContextProvider>
   );
 }
 
